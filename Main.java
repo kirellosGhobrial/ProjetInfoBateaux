b68e74c75ca713f7d840d55fd5693bf36389cf04
kirellosGhobrial <57769412+kirellosGhobrial@users.noreply.github.com>
2020-05-04 12:14:58 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,8 @@
+import java.util.scanner
+
 public class Main{
 	public static void main(String[] args){
-		System.out.println("hiiiii");
-		System.out.println("hiiiii");
-		System.out.println("hiiiii");
-		System.out.println("ok hiiiii");
-		System.out.println("amine");
+		
 	}
 
 
